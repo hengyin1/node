968f3ad61db5b9c9deba85a73dce1ce44db2f650
yinheng <herman.heng.yin@gmail.com>
2019-11-07 23:54:02 +0800

    list

--- list/node/index.js
+++ list/node/index.js
@@ -7,11 +7,18 @@ require('@babel/register')({
 });
 const ReactDOMServer  = require('react-dom/server');
 
+const App = require('./app,jsx');
+
 app.use(mount('/static', static(__dirname + '/source/static/')));
+const template = require('./template')(__dirname + '/index.htm');
 
 app.use(
   async (ctx) => {
-    
+    const filtType = +(ctx.query.filt || 0);
+    const sortType = +(ctx.query.sort || 0);
+    const reactData = await getData(sortType, filtType);
+    ctx.status = 200;
+    ctx.body = ReactDOMServer.renderToString(App(reactData));
   }
 );
 
