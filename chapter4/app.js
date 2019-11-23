83bde317379fba75e3ecea31a1e8192e7d8ab2d3
yinheng <herman.heng.yin@gmail.com>
2019-11-23 11:41:58 +0800

    cluster

--- chapter4/app.js
+++ chapter4/app.js
@@ -1,9 +1,17 @@
 const http = require('http');
 const fs = require('fs');
 
+const leak = [];
 http.createServer(function (req, res) {
   res.writeHead(200);
-  res.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
+
+  setTimeout(() => {
+    // console.log(window.location.href);
+    const result = fs.readFileSync(__dirname + '/index.html', 'utf-8');
+
+    leak.push(result);
+    res.end(result);
+  }, 50);
 }).listen(3000, () => {
-  console.log('listening 3000');
+  console.log('listening 3000'); 
 })
--- chapter4/index.js
+++ chapter4/index.js
@@ -2,8 +2,29 @@
 
  if (cluster.isMaster) {
    cluster.fork();
-   cluster.fork();
-   cluster.fork();
+  //  cluster.fork();
+  //  cluster.fork();
+
+  cluster.on('exit', () => {
+    setTimeout(() => {
+      cluster.fork();
+    }, 5000);
+  })
  } else {
    require('./app.js');
+
+   process.on('uncaughtException', (err) => {
+     console.error(err);
+     process.exit(1);
+   })
+   
+   setInterval(() => {
+     const rss = process.memoryUsage().rss;
+     console.log('rss', rss);
+     
+     if (rss > 734003200) {
+       console.log('oom');
+       process.exit(1);
+     }
+   }, 5000)
  }
