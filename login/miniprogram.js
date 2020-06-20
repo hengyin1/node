0f9af5a0d5cf015b9496313b61400423758409fd
尹恒 <yinheng@yinhengdeMacBook-Pro.local>
2020-06-20 22:30:17 +0800

    login

--- login/miniprogram.js
+++ login/miniprogram.js
@@ -16,7 +16,7 @@ app.use(
     const query = ctx.query;
     console.log('query', query);
     const js_code = query.js_code;
-    const chunk = await request(`https://api.weixin.qq.com/sns/jscode2session?appid=wx67fd121a844a87de&secret=ebf3061daaad76849da815982ded7447&js_code=${js_code}&grant_type=authorization_code`);
+    const chunk = await request(`https://api.weixin.qq.com/sns/jscode2session?appid=&secret=&js_code=${js_code}&grant_type=authorization_code`);
     ctx.status = 200;
     ctx.body = chunk;
   })
