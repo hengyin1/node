374c991183335016943b5080077d5435757b6c3a
尹恒 <yinheng@yinhengdeMacBook-Pro.local>
2020-08-23 20:13:34 +0800

    graphics

--- graphics/regularShape/app.js
+++ graphics/regularShape/app.js
@@ -19,4 +19,23 @@ function regularShape(edges = 3, x, y, step) {
   return ret;
 }
 
-draw(regularShape(3, 128, 128, 100), ctx);
+const points = regularShape(3, 128, 128, 100);
+draw(points, ctx);
+
+
+const {left: _left, top: _top} = canvas.getBoundingClientRect();
+
+canvas.addEventListener('mousemove', (evt) => {
+  const {x, y} = evt;
+  // 坐标转换
+  const offsetX = x - _left;
+  const offsetY = y - _top;
+
+  ctx.clearRect(-256, -256, 512, 512);
+
+  if(ctx.isPointInPath(offsetX, offsetY)) {
+    draw(points, ctx, {fillStyle: 'green'});
+  } else {
+    draw(points, ctx, {fillStyle: 'red'});
+  }
+});
