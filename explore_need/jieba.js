689ff1bd04be210ca1476064dcc731577423be91
尹恒 <herman.heng.yin@gmail.com>
2020-10-13 08:17:09 +0800

    explore_need

--- explore_need/jieba.js
+++ explore_need/jieba.js
@@ -1,13 +1,26 @@
 const fs = require('fs');
+const iconv = require('iconv-lite');
 const parse = require("csv-parse");
 const nodejieba = require("nodejieba");
 
-fs.readFile(__dirname + '/data/data2.csv', 'utf-8', (err, res) => {
-  console.log(res);
+// fs.createReadStream(__dirname + '/data/data1.csv').pipe(iconv.decodeStream('gbk')).pipe(iconv.encodeStream('utf8')).pipe(fs.createWriteStream(__dirname + '/data/data4.csv'));
+
+fs.readFile(__dirname + '/data/data4.csv', 'utf-8', (err, res) => {
+  // console.log(res);
   parse(res, {
-    bom: true
+    from_line: 3
   }, function(err, output){
-    console.log(err);
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    output = output.map(item => {
+      const words = nodejieba.tag(item).filter(item => item.tag == 'n' || item.tag == 'eng');
+      const word = words.reduce((pre, cur) => pre + cur.word, '');
+      return word;
+    });
+
     console.log(output);
   })
 })
