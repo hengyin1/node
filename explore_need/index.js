3ee6fb5f7045cc9efb67260d813b4735c5109652
尹恒 <herman.heng.yin@gmail.com>
2020-10-16 22:49:15 +0800

    explore_need

---
+++ explore_need/index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const iconv = require('iconv-lite');
+const parse = require("csv-parse");
+const nodejieba = require("nodejieba");
+
+// fs.createReadStream(__dirname + '/data/data1.csv').pipe(iconv.decodeStream('gbk')).pipe(iconv.encodeStream('utf8')).pipe(fs.createWriteStream(__dirname + '/data/data4.csv'));
+
+fs.readFile(__dirname + '/data/input1.csv', 'utf-8', (err, res) => {
+  // console.log(res);
+  parse(res, {
+    from_line: 3
+  }, function(err, output){
+    if (err) {
+      console.log(err);
+      return;
+    }
+    
+    output = output.map(item => {
+      // const words = nodejieba.tag(item[0]).filter(item => item.tag == 'n' || item.tag == 'eng' || item.tag == 'nr' || item.tag == 'nz' || item.tag == 'x');
+      // const word = words.reduce((pre, cur) => pre + cur.word, '');
+      // return word;
+      
+      return  nodejieba.tag(item[0])
+              .filter(item => item.tag != 'v' && item.tag != 'r' && item.tag != 'l' && item.tag != 'ul' && item.tag != 'c' && item.tag != 'uj' && item.tag != 'd')
+              .map(item => item.word);
+    }).flat();
+    // console.log(output);
+
+    const outputObj = {};
+    output.forEach(element => {
+      if (outputObj.hasOwnProperty(element)) {
+        outputObj[element] += 1;
+      } else {
+        outputObj[element] = 1;
+      }
+    })
+    // console.log(outputObj);
+
+    const compaire = (a, b) => {
+      return b[1] - a[1];
+    }
+
+    let outputStr = '\ufeff';
+    for (const [key, value] of Object.entries(outputObj).sort(compaire)) {
+      outputStr += `${key},${value}\n`;
+    }
+    console.log(outputStr);
+
+    fs.writeFile(__dirname + '/data/output1.csv', outputStr, {encoding: 'utf8'}, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    })
+  })
+})
+
