1453994beb6353e51a10f45b2475e807fd8c3f1f
尹恒 <herman.heng.yin@gmail.com>
2020-10-18 22:40:56 +0800

    explore_need

--- explore_need/classify.js
+++ explore_need/classify.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const parse = require("csv-parse");
 const nodejieba = require("nodejieba");
 
-fs.readFile(__dirname + '/data/zenme_tupian.csv', 'utf-8', (err, res) => {
+fs.readFile(__dirname + '/data/zenme_pdf.csv', 'utf-8', (err, res) => {
   // console.log(res);
   parse(res, {
     from_line: 3,
@@ -12,33 +12,83 @@ fs.readFile(__dirname + '/data/zenme_tupian.csv', 'utf-8', (err, res) => {
       console.log(err);
       return;
     }
-
-    output = output.map(item => item[0]);
     
-    let output_cut = output.map(item => {
-      return  nodejieba.tag(item).map(item => item.word);
-    })
-    // console.log(output_cut);
-
-    const vecIndex = {};
-    let vecCounter = 1;
-    output_cut.flat().forEach(element => {
-      if (!vecIndex.hasOwnProperty(element)) {
-        vecIndex[element] = vecCounter++;
-      }
-    })
-    // console.log(vecIndex);
-
-    output_cut = output_cut.map((element, index) => {
+    output = output.map(item => {
+      const tags =['r', 'i', 'p', 'uj', 'x', 'f', 'b', 'm', 'a', 'd', 'l', 'c'];
       return {
-        word: output[index],
-        vec: element.map(item => vecIndex[item]).reduce((pre, cur) => pre + cur, 0) / element.length
+        word: item[0],
+        cut: Array.from(new Set(nodejieba.tag(item[0]).filter(item => !tags.includes(item.tag)).map(item => item.word)))
       }
     })
+    // console.log(output);
 
-    output_cut = output_cut.filter(item => Math.abs(item.vec - 3) <= 3);
-    console.log(output_cut.length);
-    console.log(output_cut);
+    let counter = 0;
+    const classify_loop = () => {
+      // counter++;
+      // if (counter > 3) return;
+
+      let classify = [];
+      let refer_cut = null;
+      let start_index = 0;
+      for (let i = 0; i < output.length; i++) {
+        if (output[i]) {
+          classify.push(output[i].word);
+          refer_cut = output[i].cut;
+          start_index = i + 1;
+          output[i] = null;
+          break;
+        }
+      }
+  
+      if (!refer_cut) return;
+  
+      for (let i = start_index; i < output.length; i++) {
+        if (!output[i]) continue;
+        const { word, cut } = output[i];
+        const vec_index = Array.from(new Set([...refer_cut, ...cut]));
+        const refer_vec = vec_index.map(item => refer_cut.includes(item) ? 1 : 0);
+        const vec = vec_index.map(item => cut.includes(item) ? 1 : 0);
+        const dot_res = dot(normalize(refer_vec), normalize(vec));
+        if (dot_res >= 0.7) {
+          output[i] = null;
+          classify.push(word);
+        }
+      }
+      // console.log(classify);
+  
+      let outputStr = '\ufeff' + `${classify.length}\n`;
+      classify.forEach(item => {
+        outputStr += `${item}\n`;
+      })
+      // console.log(outputStr);
+  
+      const file_name = refer_cut.join('_');
+      fs.writeFile(__dirname + `/data/classify/${file_name}.csv`, outputStr, {encoding: 'utf8'}, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+        classify_loop();
+      })
+    }
+
+    classify_loop();
   })
 })
+
+function normalize(out) {
+  let len = out.reduce((pre, cur) => pre + cur * cur, 0);
+  if (len > 0) {
+      len = 1 / Math.sqrt(len);
+  }
+
+  return out.map(item => item * len);
+}
+
+function dot(a, b) {
+  let out = 0;
+  for (let i = 0; i < a.length; i++) {
+    out += a[i] * b[i];
+  }
+
+  return out;
+}
 
