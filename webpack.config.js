19b756401953c8a2df7f5f0118f870af8f0e6969
尹恒 <herman.heng.yin@gmail.com>
2020-10-26 21:53:42 +0800

    graphics

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const webpack = require('webpack');
+const path = require('path');
+
+function sourceMapLoader(loaderName) {
+  return {
+    loader: loaderName,
+    options: {
+      sourceMap: true
+    }
+  }
+}
+
+const config = {
+  devtool: 'cheap-eval-source-map',
+
+  // context: path.join(__dirname, 'src'),
+
+  entry: "./graphics/3d-model/axis-angle.js",
+
+  output: {
+    path: path.resolve(__dirname, 'graphics/3d-model/dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+
+      {
+          test: /\.js|jsx$/,
+          exclude: /node_modules/,
+          use: [
+              'babel-loader',
+          ]
+      },
+
+      {
+          test: /\.css$/,
+          use: [
+              'style-loader',
+              sourceMapLoader('css-loader')
+          ]
+      },
+
+      {
+          test: /\.scss/,
+          use: [
+              'style-loader',
+              sourceMapLoader('css-loader'),
+              sourceMapLoader('sass-loader')
+          ]
+      },
+
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.scss', '.css'],
+
+    modules: ["node_modules"],
+
+  },
+
+  plugins: [
+
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    }),
+
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'graphics/3d-camera/dist'),
+    compress: true,
+    hot: true,
+    port: 9000,
+    // index: 'index.html'
+  }
+}
+
+
+module.exports = config;
