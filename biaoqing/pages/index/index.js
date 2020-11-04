92acc59828acee9f5bffb1db1a00a093b170786d
尹恒 <herman.heng.yin@gmail.com>
2020-11-04 08:24:37 +0800

    biaoqing

--- biaoqing/pages/index/index.js
+++ biaoqing/pages/index/index.js
@@ -3,6 +3,7 @@
 import { tabs, templates, faces } from '../../utils/localdata.js'
 
 const app = getApp()
+const { windowWidth, windowHeight } = wx.getSystemInfoSync();
 
 Page({
   data: {
@@ -55,13 +56,47 @@ Page({
     const { index } = e.currentTarget.dataset;
     const item = this.data.list[index];
     this.getImageInfo(item.url).then(res => {
+      const { width, height, path } = res;
       if (this.data.selectedUpTab == 'tem') {
-        this.setTemSize();
+        this.tem_item = item;
+        this.setTemSize({width, height, path});
       } else {
-        this.setFaceSize();
+        this.setFaceSize({width, height, path});
       }
     }, () => {
 
+    })
+  },
+  setTemSize: function (pic) {
+    const ratio = pic.width / pic.height;
+    if (ratio > windowWidth / 200) {
+      pic.width = 0.9 * windowWidth;
+      pic.height = pic.width / ratio;
+    } else {
+      pic.height = 200;
+      pic.width = pic.height * ratio;
+    }
+    this.setData({
+      temInfo: pic
+    })
+  },
+  setFaceSize: function (pic) {
+    const ratio = pic.width / pic.height;
+    const { face_x, face_y, face_width, face_height } = this.tem_item;
+    console.log(face_x, face_y, face_width, face_height);
+    
+    if (ratio > face_width / face_height) {
+      pic.width = face_width;
+      pic.height = pic.width / ratio;
+    } else {
+      pic.height = face_height;
+      pic.width = pic.height * ratio;
+    }
+    pic.left = face_x;
+    pic.top = face_y;
+    
+    this.setData({
+      faceInfo: pic
     })
   },
   getImageInfo: function (src) {
