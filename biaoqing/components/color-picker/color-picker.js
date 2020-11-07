a1e23e73974510c32c04b44e38a848958285854e
尹恒 <herman.heng.yin@gmail.com>
2020-11-07 12:10:03 +0800

    biaoqing

--- biaoqing/components/color-picker/color-picker.js
+++ biaoqing/components/color-picker/color-picker.js
@@ -1,4 +1,5 @@
 const { windowWidth } = wx.getSystemInfoSync()
+let context
 
 Component({
 	properties: {
@@ -15,23 +16,43 @@ Component({
 	},
 	methods: {
 		initStage: function() {
-			const context = wx.createCanvasContext('colorpicker');
-			context.drawImage('./static/color.jpg', 0, 0, this.data.canvas.width, this.data.canvas.height);
-			context.draw();
+			const query = wx.createSelectorQuery().in(this);
+			query.select('#colorpicker')
+      .fields({ node: true })
+      .exec(res => {
+        const canvas = res[0].node;
+				context = canvas.getContext('2d');
+				
+        canvas.width = this.data.canvas.width;
+				canvas.height = this.data.canvas.height;
+
+				const img = canvas.createImage();
+				img.onload = () => {
+					context.drawImage(img, 0, 0, this.data.canvas.width, this.data.canvas.height);
+				}
+				img.src = './static/color.jpg';
+      })
+
+			// const context = wx.createCanvasContext('colorpicker');
+			// context.drawImage('./static/color.jpg', 0, 0, this.data.canvas.width, this.data.canvas.height);
+			// context.draw();
+		},
+		touchstart: function(e) {
+			this.touch(e);
 		},
 		touchmove: function(e) {
+			this.touch(e);
+		},
+		touch: function (e) {
 			const { x, y } = e.touches[0];
 			if (x >= 0 && x <= this.data.canvas.width && y >= 0 && y <= this.data.canvas.height) {
-				const [r, g, b] = this.data.ctx.getImageData(x, y, 1, 1).data;
-				this.triggerEvent("touchmove", {
+				const [r, g, b] = context.getImageData(x, y, 1, 1).data;
+				this.triggerEvent("touch", {
 					r: r,
 					g: g,
 					b: b
 				})
 			}
-		},
-		touchend: function() {
-			this.triggerEvent("touchend", {});
 		}
 	}
 })
--- biaoqing/pages/addtext/addtext.js
+++ biaoqing/pages/addtext/addtext.js
@@ -47,9 +47,24 @@ Page({
       [`texts[${index}].value`]: e.detail.value
     })
   },
-  toggle: function () {
+  toggle: function (e) {
+    const { index } = e.currentTarget.dataset;
     this.setData({
-      isShow: !this.data.isShow
+      isShow: !this.data.isShow,
+      toggleIndex: index
+    })
+  },
+  touchColor: function (e) {
+    const { r, g, b } = e.detail;
+    if (r == undefined || g == undefined || b == undefined) return;
+    const color = `rgb(${r}, ${g}, ${b})`;
+    this.setData({
+      [`texts[${this.data.toggleIndex}].color`]: color
+    })
+  },
+  changeFontSize: function (e) {
+    this.setData({
+      [`texts[${this.data.toggleIndex}].fontSize`]: e.detail.value
     })
   },
   onShareAppMessage: function () {
