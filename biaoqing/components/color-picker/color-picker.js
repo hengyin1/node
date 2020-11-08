e5cbf50593e9177850053aad2d7187343fffb3f6
尹恒 <herman.heng.yin@gmail.com>
2020-11-08 11:03:14 +0800

    biaoqing

--- biaoqing/components/color-picker/color-picker.js
+++ biaoqing/components/color-picker/color-picker.js
@@ -16,26 +16,26 @@ Component({
 	},
 	methods: {
 		initStage: function() {
-			const query = wx.createSelectorQuery().in(this);
-			query.select('#colorpicker')
-      .fields({ node: true })
-      .exec(res => {
-        const canvas = res[0].node;
-				context = canvas.getContext('2d');
+			// const query = wx.createSelectorQuery().in(this);
+			// query.select('#colorpicker')
+      // .fields({ node: true })
+      // .exec(res => {
+      //   const canvas = res[0].node;
+			// 	context = canvas.getContext('2d');
 				
-        canvas.width = this.data.canvas.width;
-				canvas.height = this.data.canvas.height;
+      //   canvas.width = this.data.canvas.width;
+			// 	canvas.height = this.data.canvas.height;
 
-				const img = canvas.createImage();
-				img.onload = () => {
-					context.drawImage(img, 0, 0, this.data.canvas.width, this.data.canvas.height);
-				}
-				img.src = './static/color.jpg';
-      })
+			// 	const img = canvas.createImage();
+			// 	img.onload = () => {
+			// 		context.drawImage(img, 0, 0, this.data.canvas.width, this.data.canvas.height);
+			// 	}
+			// 	img.src = './static/color.jpg';
+      // })
 
-			// const context = wx.createCanvasContext('colorpicker');
-			// context.drawImage('./static/color.jpg', 0, 0, this.data.canvas.width, this.data.canvas.height);
-			// context.draw();
+			const context = wx.createCanvasContext('colorpicker', this);
+			context.drawImage('./static/color.jpg', 0, 0, this.data.canvas.width, this.data.canvas.height);
+			context.draw();
 		},
 		touchstart: function(e) {
 			this.touch(e);
@@ -46,12 +46,28 @@ Component({
 		touch: function (e) {
 			const { x, y } = e.touches[0];
 			if (x >= 0 && x <= this.data.canvas.width && y >= 0 && y <= this.data.canvas.height) {
-				const [r, g, b] = context.getImageData(x, y, 1, 1).data;
-				this.triggerEvent("touch", {
-					r: r,
-					g: g,
-					b: b
-				})
+				wx.canvasGetImageData({
+					canvasId: 'colorpicker',
+					x: x,
+					y: y,
+					width: 1,
+					height: 1,
+					success: res => {
+						const [r, g, b] = res.data;
+						this.triggerEvent("touch", {
+							r: r,
+							g: g,
+							b: b
+						})
+					}
+				}, this)
+
+				// const [r, g, b] = context.getImageData(x, y, 1, 1).data;
+				// this.triggerEvent("touch", {
+				// 	r: r,
+				// 	g: g,
+				// 	b: b
+				// })
 			}
 		}
 	}
