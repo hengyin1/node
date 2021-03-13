1a8289ffd86008836f145c153da7cb6c7d0a2095
尹恒 <herman.heng.yin@gmail.com>
2021-03-13 10:16:20 +0800

    delete

--- katong/siteinfo.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    name: "萤火小程序商城",
-    siteroot: "https://mh.bimeijie.top/"
-};
--- katong/utils/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    adUnitId: "",
-    shenhe: new Date(2020, 10, 11) > new Date()
-}
-module.exports = {
-    config: config,
-    API_HOST: "https://weapp.faxingwu.com/index.php?r=",
-    URL: "https://weapp.faxingwu.com"
-};
