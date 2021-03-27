088369eb3e491111b0404f43f93863498e8011a7
尹恒 <herman.heng.yin@gmail.com>
2021-03-27 16:07:07 +0800

    my-resume

---
+++ my-resume/webpack.dev.js
@@ -0,0 +1,120 @@
+const glob = require('glob');
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLInlineCSSWebpackPlugin = require('html-inline-css-webpack-plugin').default;
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const setMPA = () => {
+  const entry = {};
+  const htmlWebpackPlugins = [];
+
+  const entryFiles = glob.sync(path.resolve(__dirname, './src/*/index.jsx'));
+  entryFiles.forEach(entryFile => {
+    const match = entryFile.match(/src\/(.*)\//);
+    if (match && match[1]) {
+      const pageName = match[1];
+      entry[pageName] = entryFile;
+  
+      htmlWebpackPlugins.push(
+        new HtmlWebpackPlugin({
+          filename: `${pageName}.html`,
+          template: `/src/${pageName}/index.html`,
+          chunks: [pageName],
+          inject: true
+        })
+      )
+    }
+  })
+
+  return { 
+    entry,
+    htmlWebpackPlugins
+  }
+}
+
+const { entry, htmlWebpackPlugins } = setMPA(); 
+
+module.exports = {
+  entry: entry,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "autoprefixer",
+                    {
+                      // 选项
+                    }
+                  ]
+                ]
+              }
+            }
+          },
+          'less-loader',
+          {
+            loader: 'px2rem-loader',
+            options: {
+              remUni: 75,
+              remPrecision: 8
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+   ...htmlWebpackPlugins,
+    new HTMLInlineCSSWebpackPlugin(),
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+  // optimization: {
+  //   minimize: true,
+  //   minimizer: [
+  //     new TerserPlugin({
+  //       // include: /\.min\.js$/
+  //     }),
+  //     new CssMinimizerPlugin()
+  //   ]
+  // }
+}
