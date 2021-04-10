a734b23812d7e044ebeff94c4f355b3e27f4bbcc
尹恒 <herman.heng.yin@gmail.com>
2021-04-10 22:10:18 +0800

    build-webpack

---
+++ build-webpack/lib/webpack.ssr.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+
+const ssrConfig = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-server.js',
+    libraryTarget: 'umd'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: 'ignore-loader'
+      },
+      {
+        test: /\.less$/,
+        use: 'ignore-loader'
+      }
+    ]
+  }
+};
+
+module.exports = merge(baseConfig, ssrConfig);
