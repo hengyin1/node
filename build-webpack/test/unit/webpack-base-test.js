3d2a28ef748f6667f50ccd113bb383cce8de7281
尹恒 <herman.heng.yin@gmail.com>
2021-04-16 22:40:43 +0800

    build-webpack

---
+++ build-webpack/test/unit/webpack-base-test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('webpack.base.js test case', () => {
+  const baseConfig = require('../../lib/webpack.base');
+  // console.log(baseConfig);
+
+  it('entry', () => {
+    assert.equal(baseConfig.entry.index, '/Users/yinheng/Desktop/code/temp/node/build-webpack/test/smoke/template/src/index/index.jsx');
+    assert.equal(baseConfig.entry.search, '/Users/yinheng/Desktop/code/temp/node/build-webpack/test/smoke/template/src/search/index.jsx');
+  });
+});
