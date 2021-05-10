e4234f68811f9c61849d6ab488174034dcf713f4
尹恒 <herman.heng.yin@gmail.com>
2021-05-10 22:18:18 +0800

    build-webpack

--- build-webpack/lib/webpack.dev.js
+++ build-webpack/lib/webpack.dev.js
@@ -1,11 +1,13 @@
 const { merge } = require('webpack-merge');
 const webpack = require('webpack');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const baseConfig = require('./webpack.base.js');
 
 const devConfig = {
   mode: 'development',
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new HardSourceWebpackPlugin()
   ],
   devServer: {
     contentBase: './dist',
