d4e7895562ae150dbca02d812b74dbd927f63127
尹恒 <herman.heng.yin@gmail.com>
2022-01-28 20:10:06 +0800

    test

---
+++ py/hello.py
@@ -0,0 +1,33 @@
+# from datetime import datetime, date
+# d = datetime.now()
+# print(d)
+# print(d.year, d.month, d.day, d.hour, d.minute, d.second)
+
+if True:
+  print("true")
+else:
+  print("false")
+
+total = 'aaa' + \
+        'bbbbb' + \
+        'cccc'
+print(total)
+
+str='123456789'
+print(len(str))
+print(str[0])
+print(str[0:-1])
+print(str[0:3])
+print(str[0:5:2])
+print(str * 2)
+print(str + "a")
+print('abc\nd')
+print(r'abc\nd')
+
+pas = input("\n\n")
+print(pas)
+
+
+  
+    
+    
