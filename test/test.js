844f20051b30ee98fa986866209306c924952d8a
heng.yin <herman.heng.yin@gmail.com>
2023-04-09 08:47:25 +0800

    translator office

--- test/test.js
+++ test/test.js
@@ -1,10 +1,22 @@
-const main = require("./main");
-// const assert = require("assert");
-const should = require('should');
-
-describe("test main.js", function (params) {
-    it('should equal 55 when n === 10', function () {
-        // assert.equal(55, main.fibonacci(10));
-        main.fibonacci(10).should.equal(55);
-    });
+const axios = require("axios");
+const formdata = {
+    scene: 'writing',
+    question: 'You:小红书风格smile冲牙器广告',
+    appid: 'wx2702272d38dcc8a2',
+    openid: 'o2SYJ5ToYMyr24OdenuT9xlVQIIQ',
+    is_vip: false
+}
+axios.post('https://www.ptydd.top/chatgpt/asking', formdata, {
+    timeout: 120000,
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+.then((response) => {
+    console.log('response', response);
+    // res.send({ success: true, data: response.data });
+})
+.catch((error) => {
+    console.log('errpr', error);
+    // res.status(500).send({ success: false, msg: error });
 });
